8a61854790753a35ea100b5f2e0f7cae589ec4b2
yongningfu <535802703@qq.com>
2017-05-04 00:50:34 +0800

    增加eslint

--- src/Router.js
+++ src/Router.js
@@ -3,20 +3,18 @@ import { connect } from 'dva/mobile';
 import {
   StyleSheet,
   Navigator,
-  Image,
-  View
 } from 'react-native';
 
 import {
   Router,
   Scene,
-  ActionConst
+  ActionConst,
 } from 'react-native-router-flux';
 
 import Page from './routes/Page';
 import Page2 from './routes/Page2';
 import TabIcon from './components/TabIcon';
-import Home from './routes/Home'
+import Home from './routes/Home';
 
 const getSceneStyle = (props, computedProps) => {
   const style = {
@@ -35,54 +33,6 @@ const getSceneStyle = (props, computedProps) => {
   return style;
 };
 
-
-class RouterComponent extends React.Component {
-
-  render() {
-    return (
-      <Router
-        getSceneStyle={getSceneStyle}
-        navigationBarStyle={styles.navBar}
-        titleStyle={styles.navBarTitle}
-      >
-        <Scene key="root">
-          {/*<Scene key="test"  component={RecordPlay} text="123"></Scene>*/}          
-          <Scene key="tabbar" tabs pressOpacity={0.8} type={ActionConst.REPLACE} >
-            <Scene
-              key="home"
-              component={Home}
-              text="首页"
-              iconImage="home"
-              icon={TabIcon}
-            />
-            <Scene
-              key="shopping"
-              component={Page2}
-              title="购物"
-              iconImage="shopping"
-              icon={TabIcon}
-            />
-            <Scene
-              key="profile"
-              component={Page}
-              iconImage="profile"
-              title="关于"
-              icon={TabIcon}
-            />
-            {/*<Scene
-              key="apiTest"
-              component={Page}
-              title="icute"
-              iconImage="profile"
-              icon={TabIcon}
-            />*/}
-          </Scene>
-        </Scene>
-      </Router>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   navBar: {
     backgroundColor: '#fff',
@@ -90,7 +40,50 @@ const styles = StyleSheet.create({
   navBarTitle: {
     color: '#fff',
     fontSize: 20,
-  }
+  },
 });
 
+function RouterComponent() {
+  return (
+    <Router
+      getSceneStyle={getSceneStyle}
+      navigationBarStyle={styles.navBar}
+      titleStyle={styles.navBarTitle}
+    >
+      <Scene key="root">
+        {/* <Scene key="test"  component={RecordPlay} text="123"></Scene> */}
+        <Scene key="tabbar" tabs pressOpacity={0.8} type={ActionConst.REPLACE} >
+          <Scene
+            key="home"
+            component={Home}
+            text="首页"
+            iconImage="home"
+            icon={TabIcon}
+          />
+          <Scene
+            key="shopping"
+            component={Page2}
+            title="购物"
+            iconImage="shopping"
+            icon={TabIcon}
+          />
+          <Scene
+            key="profile"
+            component={Page}
+            iconImage="profile"
+            title="关于"
+            icon={TabIcon}
+          />
+          {/* <Scene
+            key="apiTest"
+            component={Page}
+            title="icute"
+            iconImage="profile"
+            icon={TabIcon}
+          /> */}
+        </Scene>
+      </Scene>
+    </Router>
+  );
+}
 export default connect()(RouterComponent);
